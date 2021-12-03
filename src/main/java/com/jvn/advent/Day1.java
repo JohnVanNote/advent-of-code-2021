dcbb131c2f68484eff484feddfe634e416212503
John Van Note <johnlvannote@protonmail.com>
2021-12-02 22:21:31 -0500

    Day 1 ish

--- src/main/java/com/jvn/advent/Day1.java
+++ src/main/java/com/jvn/advent/Day1.java
@@ -22,6 +22,20 @@ public class Day1 {
     return numberOfDepthIncreases;
   }
 
+  public int numberOfSlidingDepthIncreases(List<Integer> depths) {
+    int numberOfSlidingDepthIncreases = 0;
+    for (int i = 0; i < depths.size() - 3; i++) {
+      if (slidingDepthTotal(depths, i+1) > slidingDepthTotal(depths, i)) {
+        numberOfSlidingDepthIncreases++;
+      }
+    }
+    return numberOfSlidingDepthIncreases;
+  }
+
+  private int slidingDepthTotal(List<Integer> depths, int startingIndex) {
+    return depths.get(startingIndex) + depths.get(startingIndex + 1) + depths.get(startingIndex + 2);
+  }
+
   public static void main(String[] args) throws IOException {
     final Day1 day1 = new Day1();
     final String inputFilePath = "src/main/resources/day1/input.txt";
@@ -30,6 +44,8 @@ public class Day1 {
           .map(Integer::parseInt)
           .collect(Collectors.toList());
       System.out.println(String.format("There are %d depth increases", day1.numberOfDepthIncreases(depths)));
+      System.out.println(String.format("There are %d sliding depth increases", day1.numberOfSlidingDepthIncreases(depths)));
+
     }
   }
 
--- src/test/java/com/jvn/advent/Day1UnitTest.java
+++ src/test/java/com/jvn/advent/Day1UnitTest.java
@@ -7,12 +7,17 @@ import org.testng.annotations.Test;
 
 public class Day1UnitTest {
 
+  private static final List<Integer> DEPTHS = ImmutableList.of(199, 200, 208, 210, 200, 207, 240, 269, 260, 263);
+
   private final Day1 day1 = new Day1();
 
   @Test
   public void numberOfDepthIncreases_standard_countsIncreases() {
-    final List<Integer> depths = ImmutableList.of(199, 200, 208, 210, 200, 207, 240, 269, 260, 263);
-    Assert.assertEquals(day1.numberOfDepthIncreases(depths) ,7);
+    Assert.assertEquals(day1.numberOfDepthIncreases(DEPTHS) ,7);
   }
 
+  @Test
+  public void numberOfSlidingDepthIncreases_standard_countsIncreases() {
+    Assert.assertEquals(day1.numberOfSlidingDepthIncreases(DEPTHS) ,5);
+  }
 }
