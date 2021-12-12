68aba22d99652589f628a26ee6ddd608be2ed387
John Van Note <johnlvannote@protonmail.com>
2021-12-12 12:08:56 -0500

    Parsing input was the worst part

---
+++ src/main/java/com/jvn/advent/day9/Day9.java
@@ -0,0 +1,55 @@
+package com.jvn.advent.day9;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Day9 {
+
+  public int lowPointSum(List<List<Integer>> heightmap) {
+    final int rowLength = heightmap.size();
+    final int columnLength = heightmap.get(0).size();
+
+    int sum = 0;
+
+    for (int i = 0; i < rowLength; i++) {
+      for (int j = 0; j < columnLength; j++) {
+        final int point = heightmap.get(i).get(j);
+        final boolean isUpLow = (i - 1) < 0 || point < heightmap.get(i - 1).get(j);
+        final boolean isDownLow = (i + 1) >= rowLength || point < heightmap.get(i + 1).get(j);
+        final boolean isLeftLow = (j - 1) < 0 || point < heightmap.get(i).get(j - 1);
+        final boolean isRightLow = (j + 1) >= columnLength || point < heightmap.get(i).get(j + 1);
+
+        if (isUpLow && isDownLow && isLeftLow && isRightLow) {
+          sum += point + 1;
+        }
+      }
+    }
+
+    return sum;
+  }
+
+  public static void main(String[] args) throws IOException {
+    final Day9 day9 = new Day9();
+    final String inputFilePath = "src/main/resources/day9/input.txt";
+
+    try (Stream<String> lines = Files.lines(Paths.get(inputFilePath))) {
+      final List<String> allLines = lines
+          .collect(Collectors.toList());
+
+      final List<List<Integer>> heightMap = new ArrayList<>();
+      for (String line : allLines) {
+        heightMap.add(line.chars()
+            .mapToObj(Character::getNumericValue)
+            .collect(Collectors.toList()));
+      }
+
+      System.out.println(String.format("There risk level is %d", day9.lowPointSum(heightMap)));
+    }
+  }
+
+}
---
+++ src/test/java/com/jvn/advent/day9/Day9UnitTest.java
@@ -0,0 +1,25 @@
+package com.jvn.advent.day9;
+
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Day9UnitTest {
+
+  private static final List<List<Integer>> HEIGHT_MAP = ImmutableList.of(
+      ImmutableList.of(2, 1, 9, 9, 9, 4, 3, 2, 1, 0),
+      ImmutableList.of(3, 9, 8, 7, 8, 9, 4, 9, 2, 1),
+      ImmutableList.of(9, 8, 5, 6, 7, 8, 9, 8, 9, 2),
+      ImmutableList.of(8, 7, 6, 7, 8, 9, 6, 7, 8, 9),
+      ImmutableList.of(9, 8, 9, 9, 9, 6, 5, 6, 7, 8)
+  );
+
+  private final Day9 day9 = new Day9();
+
+  @Test
+  public void lowPointSum_example_example() {
+    Assert.assertEquals(day9.lowPointSum(HEIGHT_MAP), 15);
+  }
+
+}
